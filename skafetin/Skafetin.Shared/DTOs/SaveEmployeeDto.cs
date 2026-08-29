b8474f2acb2ce58eb8098de229fa9121a73d1ea3
VatroslavBB <vatrobb@gmail.com>
2026-08-29 21:57:47 +0200

    added employee dtos

---
+++ skafetin/Skafetin.Shared/DTOs/SaveEmployeeDto.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Skafetin.Shared.DTOs;
+
+public class SaveEmployeeDto
+{
+    [Required(ErrorMessage = "Naziv je obavezan")]
+    [StringLength(50, ErrorMessage = "Predugo ime")]
+    public string FirstName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Naziv je obavezan")]
+    [StringLength(50, ErrorMessage = "Predugo prezime")]
+    public string LastName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Naziv je obavezan")]
+    [StringLength(100, ErrorMessage = "Predugo ime email adrese")]
+    public string Email { get; set; } = string.Empty;
+
+    [StringLength(20, ErrorMessage = "Predug telefonski broj")]
+    public string? Phone { get; set; }
+
+    [StringLength(50, ErrorMessage = "Predugo ime radne pozicije")]
+    public string? JobTitle { get; set; }
+
+    [Range(1, int.MaxValue, ErrorMessage = "Lokacija obavezna")]
+    public int LocationId { get; set; }
+
+    public bool IsActive { get; set; } = true;
+}
+
