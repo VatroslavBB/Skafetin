fb254b61cb8d8ae3bdd067d14bc58be7c1197043
VatroslavBB <vatrobb@gmail.com>
2026-08-30 20:55:35 +0200

    login dtos

---
+++ skafetin/Skafetin.Shared/DTOs/LoggedUserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Skafetin.Shared.DTOs;
+
+public class LoggedUserDto
+{
+    public int Id { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string DisplayName { get; set; } = string.Empty;
+    public List<string> Roles { get; set; } = new();
+    public int? EmployeeId { get; set; }
+    public int? LocationId { get; set; }
+}
+
