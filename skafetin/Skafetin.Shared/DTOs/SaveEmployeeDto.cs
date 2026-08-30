09d4dd10b501afecb4481073ded43a4afbac40ce
VatroslavBB <vatrobb@gmail.com>
2026-08-30 09:23:10 +0200

    employee endpoints

--- skafetin/Skafetin.Shared/DTOs/SaveEmployeeDto.cs
+++ skafetin/Skafetin.Shared/DTOs/SaveEmployeeDto.cs
@@ -4,25 +4,26 @@ namespace Skafetin.Shared.DTOs;
 
 public class SaveEmployeeDto
 {
-    [Required(ErrorMessage = "Naziv je obavezan")]
-    [StringLength(50, ErrorMessage = "Predugo ime")]
+    [Required(ErrorMessage = "Naziv je obavezan.")]
+    [StringLength(50, ErrorMessage = "Predugo ime.")]
     public string FirstName { get; set; } = string.Empty;
 
-    [Required(ErrorMessage = "Naziv je obavezan")]
-    [StringLength(50, ErrorMessage = "Predugo prezime")]
+    [Required(ErrorMessage = "Naziv je obavezan.")]
+    [StringLength(50, ErrorMessage = "Predugo prezime.")]
     public string LastName { get; set; } = string.Empty;
 
-    [Required(ErrorMessage = "Naziv je obavezan")]
-    [StringLength(100, ErrorMessage = "Predugo ime email adrese")]
+    [Required(ErrorMessage = "Naziv je obavezan.")]
+    [StringLength(100, ErrorMessage = "Predugo ime email adrese.")]
+    [EmailAddress(ErrorMessage = "Potreban je validan format email adrese.")]
     public string Email { get; set; } = string.Empty;
 
-    [StringLength(20, ErrorMessage = "Predug telefonski broj")]
+    [StringLength(20, ErrorMessage = "Predug telefonski broj.")]
     public string? Phone { get; set; }
 
-    [StringLength(50, ErrorMessage = "Predugo ime radne pozicije")]
+    [StringLength(50, ErrorMessage = "Predugo ime radne pozicije.")]
     public string? JobTitle { get; set; }
 
-    [Range(1, int.MaxValue, ErrorMessage = "Lokacija obavezna")]
+    [Range(1, int.MaxValue, ErrorMessage = "Lokacija obavezna.")]
     public int LocationId { get; set; }
 
     public bool IsActive { get; set; } = true;
